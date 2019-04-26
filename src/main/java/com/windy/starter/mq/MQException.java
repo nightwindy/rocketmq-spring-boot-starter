bd56299390e4247bbf35f5775900128991b5fb75
windy <chaofeng@wisedu.com>
2019-04-26 16:23:39 +0800

    update

---
+++ src/main/java/com/windy/starter/mq/MQException.java
@@ -0,0 +1,13 @@
+package com.windy.starter.mq;
+
+/**
+ * @Author: windy
+ * @Date: 2019-04-25 10:41
+ * @Version 1.0
+ * @mail nightwindy163@gmail.com
+ */
+public class MQException extends RuntimeException {
+    public MQException(String msg) {
+        super(msg);
+    }
+}
